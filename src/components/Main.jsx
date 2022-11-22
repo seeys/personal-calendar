c434a192845a6f6d78ceb53add30ce56f036791a
siha <es2008e@naver.com>
2022-11-22 15:24:19 +0900

    Feat : 날짜 클릭 시 모달창 생성

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,32 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import "./Main.css";
+import ModalWindow from "./ModalWindow";
 const Container = styled.div`
   width: 800px;
   height: 600px;
   margin: 0 auto;
 `;
 const Main = () => {
-  const handleDateClick = (event) => {
-    console.log(event.dayEl);
+  const [open, setOpen] = useState(false);
+  const handleShow = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
   };
   return (
     <Container>
       <FullCalendar
         defaultView="dayGridMonth"
         plugins={[dayGridPlugin, interactionPlugin]}
-        dateClick={handleDateClick}
+        dateClick={handleShow}
         events={[{ title: "event1", date: "2022-11-24" }]}
       />
+      <ModalWindow open={open} close={handleClose} />
     </Container>
   );
 };
---
+++ src/components/ModalWindow.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+import "bootstrap/dist/css/bootstrap.min.css";
+const ModalWindow = ({ open, close }) => {
+  return (
+    <div>
+      {open ? (
+        <Modal show={open} onHide={close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={close}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={close}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      ) : null}
+    </div>
+  );
+};
+
+export default ModalWindow;
